30677b013396538d77be6bb59da32ae1f770e56e
Victor Jose Arana Rodriguez <victorr@backbase.com>
2023-02-01 13:00:50 -0600

    final version

--- Main.java
+++ Main.java
@@ -8,27 +8,23 @@ class Main {
         // 1. Read URL
         var scanner = new Scanner(System.in);
         String url = scanner.nextLine();
-        System.out.println("URL: " + url);
         // 2. Parse and build parameter Map
         String parameters = url.split("\\?")[1];
-        System.out.println("Parameters: " + parameters);
         String[] paramArray = parameters.split("&");
-        System.out.println(Arrays.deepToString(paramArray));
-        LinkedHashMap<String, String> paramMap = new LinkedHashMap<String, String>();
+        Map<String, String> paramMap = new LinkedHashMap<String, String>();
         for(int i = 0; i < paramArray.length; i++) {
             paramMap.put(paramArray[i].split("=")[0], 
                 paramArray[i].split("=").length > 1 ? 
                     paramArray[i].split("=")[1]: "not found");
         }
 
-        System.out.println("Parameter Map: " + paramMap);
         // 3. Print Parameter Map        
-        printMap(paramMap);
+        for(Map.Entry<String, String> entry: paramMap.entrySet()){
+            System.out.printf("%s : %s%n", entry.getKey(), entry.getValue()); 
+        } 
+        if (paramMap.containsKey("pass")) {
+            System.out.printf("password : " + paramMap.get("pass")); 
+        }
     }
 
-    static void printMap(LinkedHashMap map) {
-        for(Map.Entry<String, String> entry: map.entrySet()){
-            System.out.printf("%s : %s", entry.getValue(), map.getKey()); 
-        }    
-    }
 }
