c1cbcc21e037091b48ea35dc29da93e9f02030cc
Victor Jose Arana Rodriguez <victorr@backbase.com>
2023-02-01 11:52:15 -0600

    :parameters: read parameters:

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
         // 1. Read URL
+        var scanner = new Scanner(System.in);
+        String url = scanner.nextLine();
+        System.out.println("URL: " + url);
         // 2. Parse and build parameter Map
+        String parameters = url.split("\\?")[1];
+        System.out.println("Parameters: " + parameters);
         // 3. Print Parameter Map        
     }
 }
