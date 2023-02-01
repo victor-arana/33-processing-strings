00968c6ca024f9ebb90cc68d3a2a6e221c754d4c
Victor Jose Arana Rodriguez <victorr@backbase.com>
2023-02-01 11:55:10 -0600

    :parameters: separate parameters

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 class Main {
     public static void main(String[] args) {
@@ -9,6 +10,8 @@ class Main {
         // 2. Parse and build parameter Map
         String parameters = url.split("\\?")[1];
         System.out.println("Parameters: " + parameters);
+        String[] paramArray = parameters.split("&");
+        System.out.println(Arrays.deepToString(paramArray));
         // 3. Print Parameter Map        
     }
 }
