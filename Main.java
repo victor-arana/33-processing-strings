ab6d29d4fc782ee097073ae2e619f95cf35a1d24
Victor Jose Arana Rodriguez <victorr@backbase.com>
2023-02-01 12:29:14 -0600

    :parameters: build Map

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.LinkedHashMap;
 
 class Main {
     public static void main(String[] args) {
@@ -12,6 +14,16 @@ class Main {
         System.out.println("Parameters: " + parameters);
         String[] paramArray = parameters.split("&");
         System.out.println(Arrays.deepToString(paramArray));
+        Map<String, String> paramMap = new LinkedHashMap<>();
+        for(int i = 0; i < paramArray.length; i++) {
+            paramMap.put(paramArray[i].split("=")[0], 
+                paramArray[i].split("=").length > 1 ? 
+                    paramArray[i].split("=")[1]: "not found");
+        }
+
+        System.out.println("Parameter Map: " + paramMap);
+
+
         // 3. Print Parameter Map        
     }
 }
